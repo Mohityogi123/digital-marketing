207720e1716f1029d126b50baefa113c1510db2c
Mohityogi123 <mohityogi.img@gmail.com>
2023-08-28 15:58:37 +0530

    faq section

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,22 @@ $(window).scroll(function() {
         a = 1;
     }
 });
+
+
+
+// show modal time 
+$(document).ready(function(){
+    showPopup();
+    });
+
+function showPopup(){
+if(localStorage.getItem('YesBtn') !== 1){
+    setTimeout(() => {
+    $('#contactus').modal('show');
+    $('#popup-yes').on('click', function(){
+        localStorage.setItem('YesBtn', 1);
+        $('#contactus').modal('hide');
+    })
+    }, 3000);
+}
+}
