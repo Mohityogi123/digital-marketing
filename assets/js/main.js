07b141067017b7f74836cf2445c45410f6499664
Mohityogi123 <mohityogi.img@gmail.com>
2023-09-01 14:13:07 +0530

    -

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,18 @@ $(window).scroll(function() {
 //     e.preventDefault();
 //     $('html, body').animate({scrollTop:0}, '300');
 // });
+(function() {
+    current_page = window.location.href;
+    navlinks = document.getElementsByClassName("nav-link");
+    active_page = document.getElementsByClassName("active")[0];
+    if (active_page) {
+        active_page.classList.remove("active");
+    }
+
+    for (i=0; i < navlinks.length; i++) {
+        if (navlinks[i].href == current_page) {
+            navlinks[i].classList.add("active");
+            break;
+        }
+    }
+})();
